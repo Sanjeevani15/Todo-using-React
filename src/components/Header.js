36b271627fcdbce4ed4c8b4c54ca7f312e3699dc
Sanjeevani Bhandari <sanjeevanibhandari3@gmail.com>
2022-03-11 15:33:45 +0530

    added files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+//responsible for the displaying the name of our app
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>TODO - LIST</h1>
+    </div>
+  )
+}
+
+export default Header;
